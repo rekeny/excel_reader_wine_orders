db95cebe541a5d653072fc35c6a8e9da5c27b06a
Fernando Requeni Nacher <fernando.requeni@outlook.com>
2020-12-27 15:06:25 +0000

    created the three containers and fixed timer problem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,14 @@ class PagesController < ApplicationController
   def home
     @items_import = ItemsImport.new
     # Filter the new entries up to 18 hours since they are created
-    @new_customer = Customer.select { |customer| timer(customer.created_at) < 18 }
+    @new_customer = Customer.select { |customer| timer(customer.created_at) < 29 }
 
   end
 
   private
 
   def timer(created)
-    return (Time.now - created).to_i
+    return ((Time.now - created) / 3600).to_i
 
   end
 end
