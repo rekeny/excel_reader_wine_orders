32dfd77e352719d39ca68c8c6efd55e57ef14ef3
Fernando Requeni Nacher <fernando.requeni@outlook.com>
2021-01-04 16:01:59 +0000

    minor updates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,8 @@ class PagesController < ApplicationController
     @items_import = ItemsImport.new
     # Filter the new entries up to 18 hours since they are created
     @new_customers = Customer.order(:created_at).select { |customer| timer(customer.created_at) < 36 }
-    @customers_unready = Customer.select { |customer| timer(customer.created_at) > 18 && customer.ready == false }
-    @customers_reports = Customer.select { |customer| customer.reports.count.positive? }
+    @customers_unready = Customer.order(:created_at).select { |customer| timer(customer.created_at) > 18 && customer.ready == false }
+    @customers_reports = Customer.order(:created_at).select { |customer| customer.reports.count.positive? }
   end
 
   private
