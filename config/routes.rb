49ce1e6e478fed3b4339472a962403e0e12e6f45
Fernando Requeni Nacher <fernando.requeni@outlook.com>
2021-02-02 22:55:27 +0000

    working sending emails

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   patch 'ready', to: 'customers#ready'
   patch 'courier', to: 'customers#courier'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
 end
