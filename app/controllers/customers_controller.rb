e1a218ad8ffe1b3067094aa2089b96c5f28dc303
Fernando Requeni Nacher <fernando.requeni@outlook.com>
2021-02-04 23:58:00 +0000

    dispatch & deliver list

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,6 @@
 class CustomersController < ApplicationController
   before_action :find_customer, only: [:show, :edit, :update]
+
   def show
     @report = Report.new
   end
@@ -12,16 +13,23 @@ class CustomersController < ApplicationController
   end
 
   def dispatched
-    customer = Customer.find(params[:format])
+    customer = find_customer_format
     dispatched = Customer.dispatched(customer.dispatched)
     customer.update(dispatched: dispatched)
     redirect_back(fallback_location: root_path)
   end
 
   def courier
-    customer = Customer.find(params[:format])
+    customer = find_customer_format
     customer.courier = params[:customer][:courier]
     redirect_back(fallback_location: root_path) if customer.save
+  end
+
+  def delivered
+    customer = find_customer_format
+    delivered = Customer.delivered(customer.delivered)
+    customer.update(delivered: delivered)
+    redirect_back(fallback_location: dispatched_path)
   end
 
   private
@@ -30,7 +38,11 @@ class CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
 
+  def find_customer_format
+    Customer.find(params[:format])
+  end
+
   def strong_params
-    params.require(:customer).permit(:date, :order_num, :name, :address1, :address2, :postcode, :town, :phone_number, :dispatched, :courier)
+    params.require(:customer).permit(:date, :order_num, :name, :address1, :address2, :postcode, :town, :phone_number, :dispatched, :courier, :delivered)
   end
 end
