a507943217d8064851fde20765965e211d7383a6
Fernando Requeni Nacher <fernando.requeni@outlook.com>
2021-03-22 23:04:36 +0000

    create customer API done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,8 @@ class Customer < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_many :products, through: :orders
   has_many :reports, dependent: :destroy
+  validates :order_num, presence: true, uniqueness: true
+  validates :name, :address1, :town, :postcode, :email, presence: true
 
   def self.dispatched(dispatched)
     !dispatched
